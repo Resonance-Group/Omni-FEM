b2654def5186fe9292031c3b7b03a994645a5a18
Phillip <phillip.mobley@rockets.utoledo.edu>
2016-03-17 16:27:13 -0400

    Restructured the source to be more readable. Renamed a file and added in more menus to the menu bar

--- UI/OmniFEM.h
+++ UI/OmniFEM.h
@@ -23,11 +23,18 @@ class OmniFEMMainFrame : public wxFrame
 public:
     OmniFEMMainFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
 private:
+    /* This section is for the File menu */
     void onNewFile(wxCommandEvent &event);
-    void OnExit(wxCommandEvent &event);
-    void OnAbout(wxCommandEvent &event);
     void OnSave(wxCommandEvent &event);
     void onSaveAs(wxCommandEvent &event);
+    
+    /* This section is for the Edit menu */
+    void onPreferences(wxCommandEvent &event);
+    
+    /* This section is for the About menu */
+    void OnAbout(wxCommandEvent &event);
+    
+    void OnExit(wxCommandEvent &event);
     wxDECLARE_EVENT_TABLE();
 };
 
@@ -37,17 +44,26 @@ enum
 {
     ID_New = 1,
     ID_Save = 2,
-    ID_SaveAs = 3
+    ID_SaveAs = 3,
+    ID_Preferences = 4
 };
 
 
 
 wxBEGIN_EVENT_TABLE(OmniFEMMainFrame, wxFrame)
+    /* This section is for teh file menu */
     EVT_MENU(ID_New,   OmniFEMMainFrame::onNewFile)
     EVT_MENU(ID_Save, OmniFEMMainFrame::OnSave)
     EVT_MENU(ID_SaveAs, OmniFEMMainFrame::onSaveAs)
-    EVT_MENU(wxID_EXIT,  OmniFEMMainFrame::OnExit)
+    
+    /* This section is for the view menu */
+    EVT_MENU(ID_Preferences, OmniFEMMainFrame::onPreferences)
+    
+    /* This section is for the ABOUT menu */
     EVT_MENU(wxID_ABOUT, OmniFEMMainFrame::OnAbout)
+    
+    EVT_MENU(wxID_EXIT,  OmniFEMMainFrame::OnExit)
+    
 wxEND_EVENT_TABLE()
 
 wxIMPLEMENT_APP(OmniFEMApp);
