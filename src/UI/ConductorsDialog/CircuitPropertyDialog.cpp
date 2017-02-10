71d2b8b21ab99e74465d895db9ccf59912d4ddd0
Phillip <phillip.mobley@rockets.utoledo.edu>
2017-02-10 18:46:46 -0500

    Continuing creating the circuit dialog

--- src/UI/ConductorsDialog/CircuitPropertyDialog.cpp
+++ src/UI/ConductorsDialog/CircuitPropertyDialog.cpp
@@ -1,3 +1,43 @@
 #include <UI/ConductorsDialog/ConductorPropertyDialog.h>
 
+circuitPropertyDialog::circuitPropertyDialog() : wxDialog(NULL, wxID_ANY, "Circuit Property")
+{
+    wxFont *font = new wxFont(8.5, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+    wxSizer *headerSizer = new wxSizer(wxHORIZONTAL);
+    wxSizer *radioSizer = new wxSizer(wxVERTICAL);
+    
+    _circuitProperty.setCurrent(0.0);
+    _circuitProperty.setName("New Circuit");
+    
+    wxStaticText *name = new wxStaticText(this, wxID_ANY, "Name: ", wxPoint(12, 15), wxSize(38, 13));
+    nameTextCtrl->Create(this, wxID_ANY, wxPoint(56, 12), wxSize(197, 20));
+
+}
+
+
+void circuitPropertyDialog::getNewCircuit(circuitProperty &newCircuit)
+{
+    
+}
+
+
+
+void circuitPropertyDialog::setCircuit(circuitProperty &circuit)
+{
+    
+}
+
+
+
+void circuitPropertyDialog::clearCircuit()
+{
+    
+}
+
+
+
+circuitPropertyDialog::~circuitPropertyDialog()
+{
+    
+}
 
