2c24bb33a5a92408937c711997327d7a5dbf87da
Phillip <phillip.mobley@rockets.utoledo.edu>
2017-04-03 23:15:49 -0400

    Moved the FEMM source files. Added in more code for rendering text. Chosse font for the textin the canvas

--- src/UI/Geometry/glText.cpp
+++ src/UI/Geometry/glText.cpp
@@ -7,12 +7,15 @@ glText::glText(physicProblems problem)
     
     FT_Library font;
     
- //   if(FT_Init_FreeType(&font))
- //   {
+    if(FT_Init_FreeType(&font))
+    {
         // Throw an error if there is something wrong
- //   }
-    FT_Face face; 
-    if(FT_New_Face(font, "fonts/arial.ttf", 0, &face))
+    }
+    
+    FT_Face face;
+    
+    // Todo: Repalce this file path with a more universal file path to ensure cross-distro compatibility
+    if(FT_New_Face(font, "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 0, &face))
     {
         // Error if something went wrong
     }
@@ -29,7 +32,7 @@ glText::glText(physicProblems problem)
         
        GLuint texture;
        glGenTextures(1, &texture);
-       glBindTexture(GL_TEXTURE_23D, texture);
+       glBindTexture(GL_TEXTURE_2D, texture);
        glTexImage2D(
             GL_TEXTURE_2D,
             0,
@@ -57,7 +60,16 @@ glText::glText(physicProblems problem)
         _characterList.insert(std::pair<GLchar, character>(c, addChar));
     }
         
-    
+    glGenVertexArrays(1, &_VAO);
+    glGenBuffers(1, &_VBO);
+    glBindVertexArray(_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     
 }
 
@@ -65,5 +77,42 @@ glText::glText(physicProblems problem)
 
 void glText::renderBlockLabelText(blockProperty &labelproperty)
 {
-    
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    // Iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++) 
+    {
+        Character ch = Characters[*c];
+
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+        // Update VBO for each character
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 0.0 },            
+            { xpos,     ypos,       0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            { xpos + w, ypos + h,   1.0, 0.0 }           
+        };
+        // Render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // Update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);    
 }
