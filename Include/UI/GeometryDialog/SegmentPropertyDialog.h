2e18f8967854e4a81c663479403abea96f41acfb
Phillip <phillip.mobley@rockets.utoledo.edu>
2017-06-11 22:35:11 -0400

    Added in additional documentation

--- Include/UI/GeometryDialog/SegmentPropertyDialog.h
+++ Include/UI/GeometryDialog/SegmentPropertyDialog.h
@@ -82,29 +82,91 @@ private:
     */ 
     wxTextCtrl *_groupTextCtrl = new wxTextCtrl();
     
+    //! Combo box used to select the boundary condition for the segment
+    /*!
+        For documentation on the wxComboBox class, refer to
+        the following link:
+        http://docs.wxwidgets.org/trunk/classwx_combo_box.html
+    */ 
     wxComboBox *_boundaryListCombo = new wxComboBox();
     
+    //! Combo box used to select the conductor associated with the segment
+    /*!
+        This is only used in electrical simulations
+        For documentation on the wxComboBox class, refer to
+        the following link:
+        http://docs.wxwidgets.org/trunk/classwx_combo_box.html
+    */ 
     wxComboBox *_conductorListCombobox = new wxComboBox();
     
+    //! Check box used to select if the program should auto mesh along the segment
+    /*!
+        For documentation on the wxCheckBox class, refer to
+        the following link:
+        http://docs.wxwidgets.org/3.1.0/classwx_check_box.html
+    */ 
     wxCheckBox *_meshSpacingAutoCheckbox = new wxCheckBox();
     
+    //! Check box used to select if the segment is a construction line
+    /*!
+        For documentation on the wxCheckBox class, refer to
+        the following link:
+        http://docs.wxwidgets.org/3.1.0/classwx_check_box.html
+    */
     wxCheckBox *_hideSegmentCheckbox = new wxCheckBox();
     
+    //! Event procedure executed when the user clicks on the mesh spacing auto check box
+    /*!
+        This function will enable to user to edit the mesh size along the line
+        \param event A required parameter in order to allow the event procedure to work 
+                    properly
+    */ 
     void onMeshCheckBox(wxCommandEvent &event);
     
 public:
-    /* The magnetic boundary list is the global boundary list.
-     * The property is the segmentProperty for the line 
-     */
+    
+    //! The constructor for the class for magnetic simulations
+    /*!
+        This constructor is used for magnetic simulations.
+        This function will create the dialog and display the widgets
+        that are relevent to magnetic simulations
+        \param par Pointer to the parent window
+        \param magneticBoundaryList Pointer to the global magnetic boundary list
+        \param property The segment property that is to be edited
+        \param isArc Set to true if editing an arc. Otherwise, set to false
+    */ 
     segmentPropertyDialog(wxWindow *par, std::vector<magneticBoundary> *magneticBoundayList, segmentProperty property, bool isArc);
     
+    //! The constructor for the class for electrical simulations
+    /*!
+        This constructor is used for electrical simulations.
+        This function will create the dialog and display the widgets
+        that are relevent to electrical simulations
+        \param par Pointer to the parent window
+        \param electricalBoundaryList Pointer to the global electrical boundary list
+        \param conductorList Pointer to the global conductor list
+        \param property The segment property that is to be edited
+        \param isArc Set to true if editing an arc. Otherwise, set to false
+    */ 
     segmentPropertyDialog(wxWindow *par, std::vector<electricalBoundary> *electricalBoundaryList, std::vector<conductorProperty> *conductorList, segmentProperty property, bool _isArc);
     
+    //! Retrieves the properties that the user set
+    /*!
+        This function depends on the problem type that was set in the 
+        constructor. The problem type will determine what widgets this
+        function should look at when called. This function
+        will retrieve all user set properties and store them 
+        in the variable property
+        \param property The variable that the program will store the 
+                        saved settigns
+    */ 
     void getSegmentProperty(segmentProperty &property);
     
+    //! The desctructor of the class
     ~segmentPropertyDialog();
 
 private:
+    //! Required function call for event procedure
     wxDECLARE_EVENT_TABLE();
 };
 
